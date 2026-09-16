c3f156b8a3b65c1159c1c78240ead6ad5ee6185a
Henrique Nishimura <114105124+HenriqueFranco1402@users.noreply.github.com>
2026-09-16 11:51:39 -0300

    primeiro_commit
    👍

--- main.pl
+++ main.pl
@@ -1,1 +1,36 @@
+disciplina(banco_de_dados, obrigatoria, 6, 2).
+disciplina(seguranca_da_informacao, obrigatoria, 4, 2).
+disciplina(experiencia_criativa, obrigatoria, 6, 1).
+disciplina(modelagem_fenomenos_fisicos, obrigatoria, 4, 3).
+disciplina(natureza_discreta, obrigatoria, 4, 3).
+disciplina(sistemas_ciberfisicos, obrigatoria, 4, 3).
+disciplina(filosofia, obrigatoria, 4, 1).
+disciplina(raciocinio_algoritmico, obrigatoria, 6, 1).
+disciplina(big_data, obrigatoria, 4, 4).
+disciplina(programacao_logica, obrigatoria, 4, 2).
+disciplina(teologia, obrigatoria, 2, 4).
+disciplina(etica, obrigatoria, 2, 2).
+%materias futuras
+disciplina(resolucao_problemas_grafos, obrigatoria, 6, 5).
+disciplina(metodos_pesquisa_cientifica, obrigatoria, 4, 5).
+disciplina(data_science, obrigatoria, 6, 7).
+disciplina(cloud_computing, obrigatoria, 4, 7).
+disciplina(arquitetura_software, obrigatoria, 4, 7).
+%eletivas
+disciplina(game_design, eletiva, 4, 6).
+disciplina(ciencias_forences, eletiva, 4, 3).
+disciplina(criacao_trilha_sonora_para_jogos, eletiva, 4, 6).
+
+%1o Pre Requisito
+prerequisito(resolucao_problemas_grafos, modelagem_fenomenos_fisicos).
+prerequisito(modelagem_fenomenos_fisicos, natureza_discreta).
+%2o Pre Requisito
+prerequisito(data_science, big_data).
+%3o Pre Requisito
+prerequisito(arquitetura_software, raciocinio_algoritmico).
+
+%
+cursou(henrique, seguranca_da_informacao).
+cursou(carolina).
+cursou(santiago).
 
